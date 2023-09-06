caa514afc41608be4f3230b2aa3aec67aef6ac43
Anders Olson <sredna43@gmail.com>
2023-09-05 22:17:16 -0500

    upgrade pico

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,1 @@
+<h1>CellarMan</h1>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import '@picocss/pico';
+	import '../app.css';
+	import '$lib/components/Header.svelte';
+	import Header from '$lib/components/Header.svelte';
+</script>
+
+<body>
+	<main class="container">
+		<Header />
+		<slot />
+	</main>
+</body>
